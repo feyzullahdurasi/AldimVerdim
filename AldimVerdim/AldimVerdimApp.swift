d715a2e59db91c942e39363658b590014ba9fa5b
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-07-14 17:07:42 +0300

    Initial Commit

---
+++ AldimVerdim/AldimVerdimApp.swift
@@ -0,0 +1,17 @@
+//
+//  AldimVerdimApp.swift
+//  AldimVerdim
+//
+//  Created by Feyzullah Durası on 14.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AldimVerdimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
