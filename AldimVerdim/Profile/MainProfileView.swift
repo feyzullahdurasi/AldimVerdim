2ea9f200a2c709e8bd990091f336f7653e9e0ab6
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-07-19 11:09:56 +0300

    Whistles pages added.

---
+++ AldimVerdim/Profile/MainProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  MainProfileView.swift
+//  AldimVerdim
+//
+//  Created by Feyzullah Durası on 18.07.2024.
+//
+
+import SwiftUI
+
+struct MainProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainProfileView()
+}
